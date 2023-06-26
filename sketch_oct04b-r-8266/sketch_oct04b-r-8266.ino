45ea7bfca95a25ea3ae10d150fb38663af7360f1
Reza <mfgnow2015@gmail.com>
2023-06-26 13:56:43 +0330

    Initial Commit

---
+++ sketch_oct04b-r-8266/sketch_oct04b-r-8266.ino
@@ -0,0 +1,91 @@
+#include <SPI.h>
+#include <Wire.h>
+
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Adafruit_BMP280.h>
+
+
+
+Adafruit_BMP280 bmp; // I2C
+
+
+
+
+//RF24 radio(12, 14, 26, 25, 27);
+RF24 radio(4, 2);
+const uint64_t address = 0xF0F0F0F0E1LL;
+int counter = 0; 
+
+ 
+String data = "reza";
+ 
+void setup() 
+{
+Serial.begin(115200);
+
+// while ( !Serial ) delay(100);   // wait for native usb
+//   Serial.println(F("BMP280 test"));
+//   unsigned status;
+//   //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+//   status = bmp.begin();
+//   if (!status) {
+//     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+//                       "try a different address!"));
+   
+//     while (1) delay(10);
+//   }
+
+//   /* Default settings from datasheet. */
+//   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+//                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+//                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+//                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+//                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+                  
+
+
+
+radio.begin();                  //Starting the Wireless communication
+radio.openWritingPipe(address); //Setting the address where we will send the data
+radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+radio.stopListening();          //This sets the module as transmitter 
+}
+ 
+void loop()
+{
+   
+  Serial.println();
+  
+  radio.write(&data, sizeof(data));
+  
+  Serial.println("Data Packet Sent");
+  Serial.println("");
+  
+  // counter++;
+  // data = String(bmp.readTemperature());
+
+
+
+  // Serial.print(F("Temperature = "));
+  //   Serial.print(bmp.readTemperature());
+  //   Serial.println(" *C");
+
+  //   Serial.print(F("Pressure = "));
+  //   Serial.print(bmp.readPressure());
+  //   Serial.println(" Pa");
+
+  //   Serial.print(F("Approx altitude = "));
+  //   Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+  //   Serial.println(" m");
+
+  //   Serial.println();
+  
+
+  
+ 
+
+ 
+  delay(500);
+
+}
---
+++ sketch_oct04c-r/sketch_oct04c-r.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ 
+RF24 radio(4, 5); // CE, CSN 
+const uint64_t address = 0xF0F0F0F0E1LL;
+
+ 
+void setup() 
+{
+Serial.begin(9600);
+// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  
+radio.begin();
+Serial.print("ADDRESS :");
+radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+radio.startListening();              //This sets the module as receiver
+}
+void loop()
+{
+if (radio.available())              //Looking for the data.
+{
+Serial.println("Radio is sniffing");
+  
+char text[32] = "";                 //Saving the incoming data
+radio.read(&text, sizeof(text));    //Reading the data
+Serial.println(text);
+ display.clearDisplay();
+
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.println("Temp:");
+   display.display();
+  display.println(text);
+   display.display();
+  
+}
+}
